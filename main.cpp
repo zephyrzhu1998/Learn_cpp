c54f3326e75d915622e19cf50744d4e9f6b152b5
Zephyr.zhu <zephyrzhu1998@gmail.com>
2023-12-13 18:08:04 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // preprocess statement
+
+int main() {        // output of main function can be omitted, it will return 0 automatically
+    std::cout << "Hello, World!" << std::endl;
+    std::cin.get(); // pause and wait for typing enter
+}
